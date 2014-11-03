c47685ea0862c8ab8b9b10baff01b7ed4de1d4a5
Niko <niko.makitalo@gmail.com>
2014-11-03 12:46:34 +0200

    removed accidental node_modules dir

---
+++ orchestratorjs/resources/actions/BLETest.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+
+  // the body
+  body: function ( d1, d2 ) {
+ 
+    
+    d1.talkingCapability.say( 'voi voi!','david','1.2');
+    d2.talkingCapability.say( 'jei!','david','1.2');
+    d1.talkingCapability.say( 'juu!','david','1.2');
+    d1.talkingCapability.say( 'ei ei!','david','1.2');
+    //d2.testCapability.test();
+  
+  }
+};
