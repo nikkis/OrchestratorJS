cef9c3f32f39b322bdfbec32cd82a6124dd9a773
Niko <niko.makitalo@tut.fi>
2017-01-19 15:58:14 +0200

    jepa

--- orchestratorjs/Public/app/js/services/socketIOService.js
+++ orchestratorjs/Public/app/js/services/socketIOService.js
@@ -1,22 +1,20 @@
 var app = angular.module('ojsConsole.services.SocketIOService', []);
 
-console.log('KYRPÄ');
 
 var ojsDeviceRegistryServicePort = 9001;
 
 app.factory('socket', function ($rootScope) {
 
-    console.log('VITTU');
 
     var socket = io.connect('http://' + hostName + ':' + pubsubPort);
 
-    console.log('SAATANA');
     return {
         on: function (eventName, callback) {
 
-            console.log('HUORA');
+
 
             socket.on(eventName, function () {
+                console.log('event received!');
                 var args = arguments;
                 $rootScope.$apply(function () {
                     callback.apply(socket, args);
